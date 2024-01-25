21b51ed375923eabaada87bb5abb0067d575b5cd
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-25 12:06:44 +0100

    dev: added header

---
+++ front/src/components/layout/HeaderLayout.vue
@@ -0,0 +1,31 @@
+<template>
+  <header>
+    <a href="#">
+      <span>Mandelbrot with WASM</span>
+    </a>
+  </header>
+</template>
+
+<style scoped lang="scss">
+header {
+  height: 3em;
+  background: #eee;
+  padding: 0 0.5em;
+  display: flex;
+  align-items: center;
+
+  a {
+    text-decoration: inherit;
+    color: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+
+    span {
+      font-size: 1.3em;
+      font-weight: bold;
+    }
+  }
+}
+</style>
