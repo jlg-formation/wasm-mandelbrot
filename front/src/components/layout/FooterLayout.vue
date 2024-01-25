8c56d2aa6e4070fcec5583cbbb29ded3977bf587
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-25 13:20:38 +0100

    dev: started to custom the front project

---
+++ front/src/components/layout/FooterLayout.vue
@@ -0,0 +1,26 @@
+<template>
+  <footer>
+    <a href="#"> Legal </a>
+    <a href="#">Github</a>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  height: 3em;
+  background: #eee;
+  padding: 0 0.5em;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  a {
+    text-decoration: inherit;
+    color: inherit;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+}
+</style>
--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,9 +1,61 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="canvas">
+      <canvas class="wasm"></canvas>
+      <canvas class="js"></canvas>
+    </div>
+    <div class="command">
+      <label>
+        <span>Iterations</span>
+        <input type="range" name="" id="" min="1" max="1000" />
+      </label>
+      <label>
+        <span>Trigger</span>
+        <input type="range" name="" id="" min="1" max="1000" />
+      </label>
+    </div>
+    <div class="stats">
+      <table>
+        <tbody>
+          <tr class="wasm">
+            <th class="name">WASM</th>
+            <td class="value">123</td>
+          </tr>
+          <tr class="js">
+            <th class="name">JS</th>
+            <td class="value">56</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </main>
 </template>
+
+<style scoped lang="scss">
+div.canvas {
+  display: flex;
+  gap: 0.5em;
+  flex-wrap: wrap;
+
+  canvas {
+    border: 0.1em solid black;
+    flex: 1;
+  }
+}
+
+div.command {
+  border: 0.1em solid black;
+
+  display: flex;
+  flex-flow: column;
+  padding: 0.5em;
+  gap: 0.5em;
+
+  label {
+    display: flex;
+    flex-flow: column;
+  }
+}
+</style>
