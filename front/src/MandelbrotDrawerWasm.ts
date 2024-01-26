e35502ce7cbe4757129ef6a719ee5a3fb9d4c995
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-26 19:05:36 +0100

    dev: added wasm

--- front/src/MandelbrotDrawerWasm.ts
+++ front/src/MandelbrotDrawerWasm.ts
@@ -5,13 +5,33 @@ import { getPoint } from './utils/image'
 import { getMandelbrotNumber } from './utils/mandelbrot.utils'
 import { get2dContext } from './utils/misc'
 
+import init from '@wasm@/release.wasm?init'
+
+interface MandelbrotWasmModule {
+  add(a: number, b: number): number
+}
+
 export class MandelbrotDrawerWasm implements MandelbrotDrawer {
+  static isInitialized = false
+  constructor() {}
+
+  async init() {
+    if (MandelbrotDrawerWasm.isInitialized) {
+      return
+    }
+    const instance = await init()
+    MandelbrotDrawerWasm.isInitialized = true
+    const result = (instance.exports as MandelbrotWasmModule).add(3, 4)
+    console.log('result: ', result)
+  }
+
   async draw(
     canvas: HTMLCanvasElement,
     viewBox: ViewBox,
     iterationMaximum: number,
     limit: number
   ): Promise<void> {
+    await this.init()
     const context = get2dContext(canvas)
     const imageData = context.createImageData(canvas.width, canvas.height)
 
