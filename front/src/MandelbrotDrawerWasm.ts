0b1988319f5815e08a208f591b8b3b2295dbdaa1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2024-01-27 18:07:25 +0100

    dev: added memory to wasm

--- front/src/MandelbrotDrawerWasm.ts
+++ front/src/MandelbrotDrawerWasm.ts
@@ -4,7 +4,20 @@ import { generatePalette, getColor } from './utils/color'
 import { getPoint } from './utils/image'
 import { get2dContext } from './utils/misc'
 
-import { getMandelbrotNumber } from '../../mandelbrot-wasm/build/release'
+const url = new URL('../../mandelbrot-wasm/build/release.wasm', import.meta.url)
+console.log('url: ', url)
+
+const instantiate = async () => {
+  const memory = new WebAssembly.Memory({ initial: 10 })
+  const module = await WebAssembly.compileStreaming(fetch(url))
+  const { exports } = await WebAssembly.instantiate(module, {
+    env: {
+      memory
+    }
+  })
+  console.log('exports: ', exports)
+  return exports
+}
 
 export class MandelbrotDrawerWasm implements MandelbrotDrawer {
   async draw(
@@ -13,6 +26,7 @@ export class MandelbrotDrawerWasm implements MandelbrotDrawer {
     iterationMaximum: number,
     limit: number
   ): Promise<void> {
+    const exports: any = await instantiate()
     const context = get2dContext(canvas)
     const imageData = context.createImageData(canvas.width, canvas.height)
 
@@ -21,7 +35,7 @@ export class MandelbrotDrawerWasm implements MandelbrotDrawer {
     const imageContentBuffer = new Uint8Array(canvas.width * canvas.height * 4)
     for (let i = 0; i < canvas.width * canvas.height; i++) {
       const c = getPoint({ index: i, canvas, viewBox })
-      const mandelbrotNumber = getMandelbrotNumber(c.x, c.y, iterationMaximum, limit)
+      const mandelbrotNumber = exports.getMandelbrotNumber(c.x, c.y, iterationMaximum, limit)
       const color = getColor(mandelbrotNumber, iterationMaximum, palette)
       const bufferIndex = i * 4
       imageContentBuffer[bufferIndex] = color[0]
